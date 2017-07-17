54167154b358dcc74bde122fe274ceecf5fd1c78
anknife <1981856494@qq.com>
2017-07-17 19:57:42 +0800

    基础模板
    基础模板

---
+++ src/components/DhtmlxGantt.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="gantt">
+      <img src="../img/gantt.png" alt="">
+    </div>
+</template>
+<style>
+
+    #gantt{
+      position: absolute;
+      width: 50px;
+      height: 50px;
+    }
+</style>
+<script>
+  export default {
+    name: 'gantt'
+  }
+</script>
