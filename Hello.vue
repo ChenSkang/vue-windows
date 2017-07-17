49136271e51affafc5ddd9817198fbd925c4eec2
wang1464226975 <1464226975@qq.com>
2017-07-17 19:31:44 +0800

    初始页面
    背景图片，主菜单

---
+++ Hello.vue
@@ -0,0 +1,38 @@
+<template>
+<div id="menu">
+  <div id="start">
+    <img src="../img/start.png"/>
+  </div>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+body{
+  background-image: url("../img/background.jpeg");
+  background-size: 100%;
+  background-repeat: no-repeat;
+}
+#menu{
+  height: 50px;
+  width: 100%;
+  background-color: black;
+  opacity: 0.85;
+  position: absolute;
+  bottom: 0px;
+}
+#start{
+  width: 60px;
+}
+  #start:hover{
+    opacity: 0.6;
+  }
+</style>
