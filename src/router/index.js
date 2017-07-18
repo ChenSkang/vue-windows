770200ae686d08a01faf6cb054b16cec4c94c4e9
wang1464226975 <1464226975@qq.com>
2017-07-18 10:57:08 +0800

    引入组件文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-
+import meau from '@/components/meau'
+import Filemanager from '@/components/Filemanager'
+import DtmlxScheduler from '@/components/DtmlxScheduler'
+import DhtmlxGantt from '@/components/DhtmlxGantt'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +13,26 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/',
+      name: 'meau',
+      component: meau
+    },
+    {
+      path: '/',
+      name: 'Filemanager',
+      component: Filemanager
+    },
+    {
+      path: '/',
+      name: 'DtmlxScheduler',
+      component: DtmlxScheduler
+    },
+    {
+      path: '/',
+      name: 'DhtmlxGantt',
+      component: DhtmlxGantt
     }
   ]
 })
