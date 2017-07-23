001e745ac156af6282e19f3d50b09bb663355ecc
anknife <1981856494@qq.com>
2017-07-23 11:11:32 +0800

    树组件和表格

--- src/components/Filemanager.vue
+++ src/components/Filemanager.vue
@@ -12,6 +12,9 @@
         <div id="hide" v-on:click="closeWindow"><li id="hide-show"><img src="../img/hide_button.png"></li></div>
       </div>
       <div class="file-button">
+        <div id="tree-title">
+          <p>{{treeMessage}}</p>
+        </div>
         <div class="more" :title="actions"></div>
         <div class="arrow-left" :title="leftView"><img src="../img/left.png" style="width: 18px; height: 18px;"></div>
         <div class="arrow-right" :title="rightView"><img src="../img/right.png" style="width: 18px; height: 18px;"></div>
@@ -31,7 +34,7 @@
             class="filter-tree"
             :data="data"
             :props="defaultProps"
-            default-expand-all
+            indent="14"
             :filter-node-method="filterNode"
             @node-click="handleNodeClick"
             ref="tree2">
@@ -39,7 +42,35 @@
         </div>
       </div>
       <div class="puton" v-on:click="letleftsee" v-show="putOnSee" :title="putOnMessage"><img src="../img/puton.png" style="width: 13px; height: 13px; "></div>
-      <div id="file-window-tableView" v-show="viewTable"></div>
+      <div id="file-window-tableView" v-show="viewTable">
+        <el-table
+          :data="tableData3"
+          border
+          height="string"
+          fit="false"
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="Name"
+            width="150%">
+          </el-table-column>
+          <el-table-column
+              prop="name"
+              label="Date"
+              width="185%">
+          </el-table-column>
+          <el-table-column
+            prop="province"
+              label="Type"
+            width="100%">
+          </el-table-column>
+          <el-table-column
+            prop="city"
+            label="Size"
+            width="100%">
+          </el-table-column>
+        </el-table>
+      </div>
       <div id="file-window-iconsView" v-show="viewIcons"></div>
     </div>
   </div>
@@ -54,7 +85,8 @@
     height: 81px;
     position: absolute;
     left: 10px;
-    top: 100px
+    top: 100px;
+    cursor: pointer;
   }
   .icon-name{
     font-size: 13px;
@@ -66,141 +98,192 @@
   }
 
   #file-window{
-    width: 650px;
-    height: 350px;
+    width: 800px;
+    height: 450px;
     background-color: white;
     border: solid 1px black;
     position: relative;
-    top: 160px;
-    left: 400px;
+    top: 80px;
+    left: 350px;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    resize: both;
+  }
+  #file-window::-webkit-scrollbar {
+    width: 10px;
+    height: 80%;
+    bottom: 0;
   }
 
   #file-title{
     height: auto;
-    font-size: 12px;
+    font-size: 13px;
     position: absolute;
-    left: 3px;
-    top: 2px;
+    left: 8px;
+    top: 5px;
   }
 
   .file-button{
-    width: 650px;
-    height: 24px;
+    width: 100%;
+    height: 30px;
     position: absolute;
-    top: 24px;
+    top: 35px;
   }
   .file-button div{
-    width: 24px;
-    height: 24px;
+    width: 30px;
+    height: 30px;
     border: 1px solid aqua;
     border-radius: 10%;
+    cursor: pointer;
   }
   .file-button img{
-    padding: 3px;
+    padding: 6px;
   }
   .more{ position: absolute;left: 10px;  }
-  .arrow-left{ position: absolute;left: 126px; }
-  .arrow-right{ position: absolute;left: 150px; }
-  .return{ position: absolute;left: 187px; }
-  .icons-view{ position: absolute;right: 34px; }
-  .table-view{ position: absolute;right: 10px; }
+  .arrow-left{ position: absolute;left: 142px; }
+  .arrow-right{ position: absolute;left: 174px; }
+  .return{ position: absolute;left: 220px; }
+  .icons-view{ position: absolute;right: 40px }
+  .table-view{ position: absolute;right: 8px; }
   .search input{
-    height: 26px;
+    height: 30px;
     border-radius: 13px;
   }
   .search{
-    width: 80px;
+    width: 12.5%;
     position: absolute;
-    right: 70px;
-    top: 24px;
+    right: 80px;
+    top: 37px;
+    cursor: pointer;
   }
 
   #file-window-left{
     background-color: honeydew;
     border: solid 1px aqua;
-    height: 293px;
-    width: 211px;
+    height: 370px;
+    width: 250px;
     position: absolute;
     bottom: 0;
   }
 
   #tree-manager{
     border-bottom: solid 1px aqua;
-    width: 211px;
-    height: 25px;
+    width: 250px;
+    height: 30px;
     position: absolute;
     left: 0;
-    bottom: 268px;
+    top: 0;
   }
   .put{
-    width: 15px;
-    height: 15px;
+    width: 20px;
+    height: 20px;
     position: absolute;
-    top: 3px;
+    top: 5px;
+    cursor: pointer;
   }
   .put:hover{ box-shadow: inset 2px 2px 0 0 aqua; }
   .puton{
-    width: 15px;
-    height: 15px;
+    width: 20px;
+    height: 370px;
     position: absolute;
     left: 0;
-    bottom: 278px;
+    top: 85px;
+    background-color: honeydew;
+    cursor: pointer;
   }
   .puton:hover{ box-shadow: inset 2px 2px 0 0 aqua; }
   .expand{
-    width: 15px;
-    height: 15px;
+    width: 20px;
+    height: 20px;
     position: absolute;
-    top: 4px;
+    top: 7px;
     right: 40px;
+    cursor: pointer;
   }
   .expand:hover{ box-shadow: inset 2px 2px 0 0 aqua; }
   .collapse{
-    width: 15px;
-    height: 15px;
+    width: 20px;
+    height: 20px;
     position: absolute;
-    top: 4px;
+    top: 7px;
     right: 10px;
+    cursor: pointer;
   }
   .collapse:hover{ box-shadow: inset 2px 2px 0 0 aqua; }
   #tree{
-    height: 268px;
-    width: 211px;
+    height: 339px;
+    width: 250px;
     position: absolute;
     bottom: 0;
+    overflow-x: hidden;
+  }
+  #tree::-webkit-scrollbar {
+    width: 10px;
+    height: 80%;
+    bottom: 0;
+  }
+  .filter-tree{
+    height: 340px;
+    width: 240px;
+    border: none;
+    position: absolute;
+    background-color: honeydew;
+  }
+  .filter-tree div{
+    float: left;
+    width: 250px;
+    text-align: left;
+  }
+
+  #tree-title{
+    display: inline-block;
+    width: 200px;
+    border: none;
+    position: absolute;
+    left: 280px;
+  }
+  #tree-title p{
+    font-size: 15px;
+    line-height: 30px;
+    float: left;
   }
 
   #file-window-tableView{
     border: solid 1px aqua;
-    height: 293px;
-    width: 431px;
+    height: 370px;
+    width: 67.5%;
     position: absolute;
     right: 0;
     bottom: 0;
-    background-color: powderblue;
   }
+  #file-window-tableView div{
+    height: 30px;
+    text-align: left;
+    line-height: 20px;
+  }
+
   #file-window-iconsView{
     border: solid 1px aqua;
-    height: 293px;
-    width: 431px;
+    height: 370px;
+    width: 67.5%;
     position: absolute;
     right: 0;
     bottom: 0;
-    background-color: orange;
   }
 
   .file-show{
-    height: 24px;
-    width: 96px;
+    height: 50px;
+    width: 120px;
     position: absolute;
     top: 0;
     right: 0;
   }
 
   .file-show div{
-    width: 32px;
+    width: 40px;
+    height: 25px;
     float: right;
-    height: auto;
+    cursor: pointer;
   }
 
   .file-show li{
@@ -212,6 +295,7 @@
   #close:hover{  background-color: red  }
 </style>
 <script>
+  let id = 1000
   export default{
     watch: {
       filterText (val) {
@@ -237,19 +321,40 @@
         expandTree: 'Expand Tree',
         collapseTree: 'Collapse Tree',
         filterText: '',
+        treeMessage: 'Files',
         data: [{
           id: 1,
-          label: '一级 1',
+          label: 'Files',
           children: [{
-            id: 4,
-            label: '二级 1-1',
+            id: 2,
+            label: 'Documents',
             children: [{
-              id: 9,
-              label: '三级 1-1-1'
+              id: 5,
+              label: 'Presentations'
             }, {
-              id: 10,
-              label: '三级 1-1-2'
+              id: 6,
+              label: 'Reports',
+              children: [{
+                id: 9,
+                label: 'China'
+              }, {
+                id: 10,
+                label: 'USA'
+              }]
             }]
+          }, {
+            id: 3,
+            label: 'Images',
+            children: [{
+              id: 7,
+              label: 'Thumbnails'
+            }, {
+              id: 8,
+              label: 'Base images'
+            }]
+          }, {
+            id: 4,
+            label: 'Video'
           }]
         }],
         defaultProps: {
@@ -283,6 +388,13 @@
       },
       handleNodeClick (data) {
         console.log(data)
+      },
+      append (store, data) {
+        store.append({ id: id++, label: 'testtest', children: [] }, data)
+      },
+
+      remove (store, data) {
+        store.remove(data)
       }
     }
   }
